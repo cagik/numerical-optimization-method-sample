29d6f17c9ababb15e06620aa59d37126ac71f0a0
cagik <1026564754@qq.com>
2022-04-07 20:52:55 +0800

    gold_search

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # numerical-optimization-method-sample
 some sample of numerical optimization method
+
+
+run command
+
+mkdir bin && cd bin
+cmake ..
+make
+./x_demo
---
+++ src/gold.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double fun(double x){
+    return 3*pow(x,2) - 2 * tan(x);
+}
+
+int main(){
+
+    double a,b,h,p,q,phia,phib,phip,phiq;
+    double t = (sqrt(5) - 1) * 0.5;
+    double end_fun, end_h;
+
+    a = 0;
+    b = 1;
+    end_fun = 1.0e-5;
+    end_h = 1.0e-4;
+
+    h = b - a;
+    p = a + (1 - t) * h;
+    q = a + t * h;
+    phia = fun(a);
+    phib = fun(b);
+    phip = fun(p);
+    phiq = fun(q);
+
+
+    int count = 0;
+    while(abs(phib - phia) > end_fun || b - a > h){
+        count++ ;
+        if(phip <= phiq){
+            b = q;
+            phib = phiq;
+            q = p;
+            phiq = phip;
+            h = b - a;
+            p = a + (1 - t)*h;
+            phip = fun(p);
+        }
+        else{
+            a = p;
+            phia = phip;
+            p = q;
+            phip = phiq;
+            h = b -a;
+            q = a + t*h;
+            phiq = fun(q);
+        }
+    }
+
+    double s = phip <= phiq ? p : q;
+    double phis = phip <= phiq ? phip : phiq;
+
+    cout << "迭代次数:" << count << endl;
+    cout << "s:" << s << endl;
+    cout << "phis:" << phis << endl;
+    cout << "区间差值" << abs(b-a) << endl;
+    cout << "fun差值" << abs(phia - phib) << endl;
+
+    return 0;
+}
